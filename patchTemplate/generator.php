243188cc9b7fcf22040e34d5c3b73dca5d232dff
Rafi Strauss <rafikis75@gmail.com>
2015-07-23 22:10:44 -0400

    Updated for slack integration

--- patchTemplate/generator.php
+++ patchTemplate/generator.php
@@ -7,13 +7,23 @@
  */
 
 
-$patchNumber = $_POST['patchNumber'];
-$patchDescription = $_POST['patchDescription'];
+if(isset($_POST['patchNumber'])) {
 
+    $patchNumber = $_POST['patchNumber'];
+    $patchDescription = $_POST['patchDescription'];
+
+}
+else {
+    $incoming = $_POST['text'];
+    $arr = explode(' ',trim($incoming));
+    $patchNumber = $arr[0];
+    unset($arr[0]);
+    $patchDescription = implode(" ", $arr);
+}
 
 $patchTemplate = "\n\n#Patch " . $patchNumber . " - " .
     $patchDescription . "\n\n\n" .
-    "INSERT INTO patchhistory VALUES (" .  $patchNumber . ", CURRENT_TIMESTAMP(), '"
+    "INSERT INTO patchhistory VALUES (" . $patchNumber . ", CURRENT_TIMESTAMP(), '"
     . $patchDescription . "' );";
 
 echo $patchTemplate;

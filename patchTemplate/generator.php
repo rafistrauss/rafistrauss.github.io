2bc897224aa15eefd3e220d2b0bc9488c7eaf485
Rafi Strauss <rafikis75@gmail.com>
2015-07-23 22:26:02 -0400

    Add auth check

--- patchTemplate/generator.php
+++ patchTemplate/generator.php
@@ -7,23 +7,30 @@
  */
 
 
-if(isset($_POST['patchNumber'])) {
+if(isset($_POST['token']) && ($_POST['token'] === "iUEeRyKIKhM8F4wngsZog4i2" || $_POST['token'] === "HLPjcEavFiSUYUHRWc1Jrku6")) {
 
-    $patchNumber = $_POST['patchNumber'];
-    $patchDescription = $_POST['patchDescription'];
 
+    if (isset($_POST['patchNumber'])) {
+
+        $patchNumber = $_POST['patchNumber'];
+        $patchDescription = $_POST['patchDescription'];
+
+    } else {
+        $incoming = $_POST['text'];
+        $arr = explode(' ', trim($incoming));
+        $patchNumber = $arr[0];
+        unset($arr[0]);
+        $patchDescription = implode(" ", $arr);
+    }
+
+    $patchTemplate = "\n\n#Patch " . $patchNumber . " - " .
+        $patchDescription . "\n\n\n" .
+        "INSERT INTO patchhistory VALUES (" . $patchNumber . ", CURRENT_TIMESTAMP(), '"
+        . $patchDescription . "' );";
+
+    echo $patchTemplate;
 }
 else {
-    $incoming = $_POST['text'];
-    $arr = explode(' ',trim($incoming));
-    $patchNumber = $arr[0];
-    unset($arr[0]);
-    $patchDescription = implode(" ", $arr);
+    sleep(5);
+    header("HTTP/1.0 403 Forbidden");
 }
-
-$patchTemplate = "\n\n#Patch " . $patchNumber . " - " .
-    $patchDescription . "\n\n\n" .
-    "INSERT INTO patchhistory VALUES (" . $patchNumber . ", CURRENT_TIMESTAMP(), '"
-    . $patchDescription . "' );";
-
-echo $patchTemplate;

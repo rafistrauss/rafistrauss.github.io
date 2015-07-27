cfb75904d230faf5048a379989516d44765f5998
rafistrauss <rafikis75@gmail.com>
2015-07-26 20:20:29 -0400

    Add token for template generator

--- patchTemplate/authorizer.php
+++ patchTemplate/authorizer.php
@@ -1,6 +1,12 @@
 <?php
 
-if(isset($_POST['token']) && ($_POST['token'] === "iUEeRyKIKhM8F4wngsZog4i2" || $_POST['token'] === "HLPjcEavFiSUYUHRWc1Jrku6")) {
+
+
+if(isset($_POST['token']) &&
+    ($_POST['token'] === "iUEeRyKIKhM8F4wngsZog4i2" ||
+        $_POST['token'] === "HLPjcEavFiSUYUHRWc1Jrku6" ||
+        $_POST['token'] === "9ntxBhATEodzozfBdLGS1oeZ")
+    ) {
 
 }
 else {

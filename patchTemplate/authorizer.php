48a2655a09b8394c4488bb49a0f2ea18d2d9e238
rafistrauss <rafikis75@gmail.com>
2015-08-09 17:28:21 -0400

    Fix authorizer

--- patchTemplate/authorizer.php
+++ patchTemplate/authorizer.php
@@ -1,10 +1,14 @@
 <?php
 
 
+require_once("tokens.inc");
+require_once("dbcreds.inc");
+
 if(isset($_POST['token']) && in_array($_POST['token'], $tokens) ) {
     $db = new PDO("mysql:dbname=$dbname;host=$host", "$user", "$pw");
 }
 else {
+    file_put_contents("log.log", "Received a token of: " . $_POST['token'] . "\n", FILE_APPEND );
     sleep(5);
     header("HTTP/1.0 403 Forbidden");
     exit;

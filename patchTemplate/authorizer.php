34fe55afe4df8e81192bb0ed5bd28ca6d51e5a14
rafistrauss <rafikis75@gmail.com>
2015-08-09 22:08:42 -0400

    Fix authorizer

--- patchTemplate/authorizer.php
+++ patchTemplate/authorizer.php
@@ -1,16 +1,17 @@
 <?php
 
-
 require_once("tokens.inc");
 require_once("dbcreds.inc");
 
-if(isset($_REQUEST['token']) && in_array($_REQUEST['token'], $tokens) ) {
+if(isset($_POST['token']) && in_array($_POST['token'], $tokens) ) {
     $db = new PDO("mysql:dbname=$dbname;host=$host", "$user", "$pw");
 }
 else {
 
-    file_put_contents("log.log", "Received a payload of: " . print_r($_POST, true) . "\n", FILE_APPEND );
+    $date = date('Y-m-d H:i:s');
+    file_put_contents("log.log", "--------------- $date - Received a payload of: " . print_r($_POST, true) . "\n", FILE_APPEND );
     sleep(5);
     header("HTTP/1.0 403 Forbidden");
+    echo "Not authorized";
     exit;
 }

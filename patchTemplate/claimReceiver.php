2926a074ac94a5f8982b384b37a2e4733b112ea8
rafistrauss <rafikis75@gmail.com>
2015-10-20 23:08:00 -0400

    Attempt at more sophisticated claiming

--- patchTemplate/claimReceiver.php
+++ patchTemplate/claimReceiver.php
@@ -12,45 +12,60 @@ require_once('claimr.php');
 $payload = $_POST['text'];
 $userName = $_POST['user_name'];
 
+$date = date("Y m D H:i:s");
+file_put_contents("log.log", "Hit at $date \n\n", FILE_APPEND);
+
 $arr = explode(' ', trim($payload));
 $entityType = $arr[0];
-$branchName = "";
+$numberOfEntitiesToClaim = 1;
 $description = "";
 if(isset($arr[1])) {
-    $branchName = $arr[1];
-}
-if(isset($arr[2])) {
-    unset($arr[0], $arr[1]);
-    $description = implode(" ", $arr);
+    if(is_numeric($arr[1])) {
+        $numberOfEntitiesToClaim = $arr[1];
+        if(isset($arr[2])) {
+            unset($arr[0], $arr[1]);
+            $description = implode(" ", $arr);
+        }
+
+    }
+    else {
+        unset($arr[0]);
+        $description = implode(" ", $arr);
+    }
+
 }
 
 $claimr = new claimr();
-$patchNumber = $claimr->claimEntity($db, $claimr->tableMapping($entityType), "$userName", $branchName);
 
-$slackBotUrl = "https://marsb.slack.com/services/hooks/slackbot?token=WLjq0DPnWUsC6H4bVQo9sE4Z&channel=%23patches";
+for($i = 0; $i < $numberOfEntitiesToClaim; $i++) {
+
+    $patchNumber = $claimr->claimEntity($db, $claimr->tableMapping($entityType), "$userName");
+
+    $slackBotUrl = "https://marsb.slack.com/services/hooks/slackbot?token=WLjq0DPnWUsC6H4bVQo9sE4Z&channel=%23patches";
 
 
-$data = "$userName claimed $entityType #$patchNumber";
+    $data = "$userName claimed $entityType #$patchNumber";
 
 // build the urlencoded data
 
-$urlenc = urlencode($data);
+    $urlenc = urlencode($data);
 
 // open connection
-$ch = curl_init();
+    $ch = curl_init();
 
 // set the url, number of POST vars, POST data
-curl_setopt($ch, CURLOPT_URL, $slackBotUrl);
-curl_setopt($ch, CURLOPT_TRANSFERTEXT, $data);
+    curl_setopt($ch, CURLOPT_URL, $slackBotUrl);
+    curl_setopt($ch, CURLOPT_TRANSFERTEXT, $data);
 
 // execute post
-$result = curl_exec($ch);
+    $result = curl_exec($ch);
 
 // close connection
-curl_close($ch);
+    curl_close($ch);
 
-$newPayload = "$patchNumber $description";
+    $newPayload = "$patchNumber $description";
 
-$_POST['text'] = $newPayload;
+    $_POST['text'] = $newPayload;
 
-include 'patchReceiver.php';
+    include 'patchReceiver.php';
+}

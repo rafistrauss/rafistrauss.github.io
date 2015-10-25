a70a12234d26a0f81b2ecedf194c9a1e582fa645
rafistrauss <rafi@proseeder.com>
2015-10-25 17:03:46 -0400

    Test

--- patchTemplate/claimReceiver.php
+++ patchTemplate/claimReceiver.php
@@ -43,7 +43,7 @@ if(isset($arr[1])) {
 }
 
 $claimr = new claimr();
-$slackBot = new \lygav\slackbot\SlackBot($slackBotUrl);
+$slackBot = new SlackBot($slackBotUrl);
 
 for($i = 0; $i < $numberOfEntitiesToClaim; $i++) {
 

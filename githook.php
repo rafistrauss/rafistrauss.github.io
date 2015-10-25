341eaff5c3ae173a4fce27647e34a6aedec53094
rafistrauss <rafi@proseeder.com>
2015-10-25 13:17:59 -0400

    Add log before pull command

--- githook.php
+++ githook.php
@@ -12,12 +12,14 @@ $date = date("Y m D H:i:s");
 
 
 $GHWH = new GitHub_WebHook();
+$logfile = 'gitlog.log';
 if($GHWH->ValidateHubSignature($secret)) {
+    file_put_contents($logfile, "Before pull at $date", FILE_APPEND);
     exec('git pull');
-    file_put_contents('gitlog.log', "Pulled at $date", FILE_APPEND);
+    file_put_contents($logfile, "Pulled at $date", FILE_APPEND);
 }
 else {
-    file_put_contents('gitlog.log', "Failed at $date", FILE_APPEND);
+    file_put_contents($logfile, "Failed at $date", FILE_APPEND);
 }
 
 

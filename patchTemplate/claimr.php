86fcc9010eb4ce8d4619d77a26e6dbfefb564bc2
Rafi <rafikis75@gmail.com>
2015-11-14 21:49:52 -0500

    More logging

--- patchTemplate/claimr.php
+++ patchTemplate/claimr.php
@@ -79,6 +79,9 @@ class claimr
             echo "There was some error";
             $this->logError( print_r(error_get_last(), true));
             $this->logError(print_r($db->errorInfo(), true));
+            $this->logError( "Query string: " . $queryString);
+            $this->logError("Query: " . $query);
+            $this->logError("Array: " . print_r($valueArray, true));
             return false;
         }
         else if($res === 0) {

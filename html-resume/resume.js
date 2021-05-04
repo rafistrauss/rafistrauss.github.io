0709967d60e04e03f4159e4b5a58078c654bc5b1
Rafi Strauss <rafikis75@gmail.com>
2021-05-03 23:31:29 -0400

    Update resume

--- html-resume/resume.js
+++ html-resume/resume.js
@@ -1,79 +1,101 @@
 const experience = [
-    {
-        title: "Software Engineer",
-        companyName: "Rubenstein Technology Group",
-        companyLink: "https://www.rubensteintech.com/",
-        startDate: "11/2016",
-        current: true,
-        location: "New York, NY",
-        tasks: [
-            {
-                description:
-                    "Implemented frontend design and functionality for client websites according to best practices, including BEM and ES6: <a href='https://200.cravath.com/'>Cravath Bicentennial</a>, <a href='https://www.bclplaw.com/'>BCLP</a>, <a href='https://www.choateia.com/'>Choate IA</a>, <a href='https://www.finnegan.com/en/'>Finnegan</a>, among others",
-                link: ""
-            },
-            {
-                description:
-                    "Wrote an internal service to integrate Trello and Request Tracker, obviating the need for duplicate content entry"
-            },
-            {
-                description:
-                    "Resolved support &amp; maintenance issues in core and project code to keep clients happy"
-            },
-            {
-                description:
-                    "Developed several internal Chrome extensions and bash scripts to aid engineer productivity and work satisfaction"
-            }
-        ]
-    },
-    {
-        title: "Software Engineer",
-        companyName: "ProSeeder Technologies Inc.",
-        startDate: "01/2015",
-        endDate: "10/2016",
-        location: "New York, NY",
-        tasks: [
-            {
-                description:
-                    "Developed new core functionality for the ProSeeder platform using a Java/JSP backend",
-                link: ""
-            },
-            {
-                description:
-                    "Implemented new designs and features to modernize the web application and improve user experience"
-            },
-            {
-                description:
-                    "Improved systems development lifecycle by automating build processes"
-            },
-            {
-                description:
-                    "Implemented SASS for product consistency, speed, and more enjoyable development"
-            },
-            {
-                description:
-                    "Worked on platform optimizations from front-end to back"
-            }
-        ]
-    },
-    {
-        title: "Co-founder & Lead Developer",
-        companyName: "Fitted Threads",
-        startDate: "10/2012",
-        endDate: "02/2015",
-        location: "New York, NY",
-        tasks: [
-            {
-                description:
-                    "Developed company website and infrastructure, including setting up account management, product display, e-commerce frontend, admin backend, and manufacturer interface",
-                link: ""
-            },
-            {
-                description:
-                    "Interfaced with tech companies such as UpCload and Poikos (Flixfit) to consider utilizing their technology for measuring customers"
-            }
-        ]
-    }
+  {
+    title: "Senior Software Engineer, Web",
+    companyName: "NBCUniversal",
+    companyLink: "https://www.nbcuniversal.com/",
+    startDate: "06/2019",
+    current: true,
+    location: "New York, NY",
+    tasks: [
+      {
+        description:
+          "Key member of the amazing team behind the widely-viewed election web pages, such as <a href='https://www.nbcnews.com/politics/2020-elections/president-results'>President Results</a>, which combined for over 100 million page views.",
+      },
+      {
+        description:
+          "Implemented and customized a lightweight CMS for editorial & product to manage the election pages without requiring releases or dev work",
+      },
+      {
+        description:
+          "Lead the initiative to onboard Telemundo to NBC News' front-end tech stack",
+      },
+
+    ],
+  },
+  {
+    title: "Software Engineer",
+    companyName: "Rubenstein Technology Group",
+    companyLink: "https://www.rubensteintech.com/",
+    startDate: "11/2016",
+    endDate: "06/2019",
+    location: "New York, NY",
+    tasks: [
+      {
+        description:
+          "Implemented frontend design and functionality for client websites according to best practices, including BEM and ES6: <a href='https://200.cravath.com/'>Cravath Bicentennial</a>, <a href='https://www.bclplaw.com/'>BCLP</a>, <a href='https://www.choateia.com/'>Choate IA</a>, <a href='https://www.finnegan.com/en/'>Finnegan</a>, among others",
+        link: "",
+      },
+      {
+        description:
+          "Wrote an internal service to integrate Trello and Request Tracker, obviating the need for duplicate content entry",
+      },
+      {
+        description:
+          "Resolved support &amp; maintenance issues in core and project code to keep clients happy",
+      },
+      {
+        description:
+          "Developed several internal Chrome extensions and bash scripts to aid engineer productivity and work satisfaction",
+      },
+    ],
+  },
+  {
+    title: "Software Engineer",
+    companyName: "ProSeeder Technologies Inc.",
+    startDate: "01/2015",
+    endDate: "10/2016",
+    location: "New York, NY",
+    tasks: [
+      {
+        description:
+          "Developed new core functionality for the ProSeeder platform using a Java/JSP backend",
+        link: "",
+      },
+      {
+        description:
+          "Implemented new designs and features to modernize the web application and improve user experience",
+      },
+      {
+        description:
+          "Improved systems development lifecycle by automating build processes",
+      },
+      {
+        description:
+          "Implemented SASS for product consistency, speed, and more enjoyable development",
+      },
+      {
+        description: "Worked on platform optimizations from front-end to back",
+      },
+    ],
+  },
+  {
+    title: "Co-founder & Lead Developer",
+    companyName: "Fitted Threads",
+    startDate: "10/2012",
+    endDate: "02/2015",
+    location: "New York, NY",
+    tasks: [
+      {
+        description:
+          "Developed company website and infrastructure, including setting up account management, product display, e-commerce frontend, admin backend, and manufacturer interface",
+        link: "",
+      },
+      {
+        description:
+          "Interfaced with tech companies such as UpCload and Poikos (Flixfit) to consider utilizing their technology for measuring customers",
+      },
+    ],
+  },
 ];
 
 const tasksTemplate = ({ description }) => `<li>

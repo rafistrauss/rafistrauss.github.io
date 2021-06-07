52387e467d3309a58d77a73b708ead4c9747c723
rafistrauss <rafikis75@gmail.com>
2021-06-07 12:48:29 -0400

    feat: use sitemap.xml instead of sitemap.txt

--- buildSitemap.js
+++ buildSitemap.js
@@ -1,6 +1,25 @@
 import fs from 'fs';
 import { items } from './src/lib/projectData.js';
 
+/**
+ * @param  {string} url
+ * @param  {string} lastModified
+ */
+const urlTemplate = (url, lastModified) => `<url>
+ <loc>${url}</loc>
+ <lastmod>${lastModified}</lastmod>
+ </url>`;
+
+/**
+ * @param  {string} sitemapEntries
+ */
+const sitemapDocTemplate = (sitemapEntries) => `<?xml version="1.0" encoding="UTF-8"?>
+ <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+	 ${sitemapEntries}
+ </urlset>`;
+
+const lastModifiedDate = new Date().toISOString().split('T')[0];
+
 const basePath = 'https://rafistrauss.com';
 
 const mainUrls = ['/', '/resume'];
@@ -9,9 +28,14 @@ const projectUrls = items.map((item) => `/projects/${item.slug}`);
 
 const allUrls = [...mainUrls, ...projectUrls];
 
-const sitemap = allUrls.map((url) => `${basePath}${url}`).join('\n');
+const sitemapUrls = allUrls
+	.map((url) => `${basePath}${url}`)
+	.map((url) => urlTemplate(url, lastModifiedDate))
+	.join('\n');
 
-fs.writeFile('static/sitemap.txt', sitemap, 'utf-8', (err) => {
+const fullSitemap = sitemapDocTemplate(sitemapUrls);
+
+fs.writeFile('static/sitemap.xml', fullSitemap, 'utf-8', (err) => {
 	if (err) {
 		console.error(err);
 	}

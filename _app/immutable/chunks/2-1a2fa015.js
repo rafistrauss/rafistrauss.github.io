4b642403d1bfda93423e998d17d475184913e8b0
rafistrauss <rafistrauss@users.noreply.github.com>
2023-01-18 16:38:15 +0000

    deploy: cf99ca21ac9a20508e454bc3155ba6e6d030ffd9

--- _app/immutable/chunks/2-1a2fa015.js
+++ _app/immutable/chunks/2-1a2fa015.js
@@ -1,1 +1,1 @@
-import{_ as r}from"./_layout-90a7f2f4.js";import{default as t}from"../components/layout.svelte-9d34d869.js";export{t as component,r as universal};
+import{_ as r}from"./_layout-da46b06b.js";import{default as t}from"../components/layout.svelte-9d34d869.js";export{t as component,r as universal};
